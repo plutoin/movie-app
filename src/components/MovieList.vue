d7884f8c03b1db269c61c1969f5fcc0df6cccc5e
plutoin <amiopy@naver.com>
2022-01-26 22:01:49 +0900

    movie container

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -4,10 +4,12 @@
       <div class="message">
         {{ message }}
       </div>
-      <MovieItem
-        v-for="movie in movies"
-        :key="movie.imdbID"
-        :movie="movie" />
+      <div class="movies">
+        <MovieItem
+          v-for="movie in movies"
+          :key="movie.imdbID"
+          :movie="movie" />
+      </div>
     </div>
   </div>
 </template>
@@ -30,3 +32,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+.container {
+  margin-top: 30px;
+  .inner {
+    background-color: $gray-200;
+    padding: 10px 0;
+    border-radius: 4px;
+  }
+  .movies {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+</style>
