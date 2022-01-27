405e13b8e45c5af5bdda47fbe23853ad98527c56
plutoin <amiopy@naver.com>
2022-01-27 23:08:08 +0900

    Footer

--- src/store/movie.js
+++ src/store/movie.js
@@ -5,7 +5,7 @@ export default {
   namespaced: true,
   state: () => ({
     movies: [],
-    message: '',
+    message: 'Search for the movie title!',
     loading: false
   }),
   getters: {},
@@ -21,6 +21,11 @@ export default {
   },
   actions: {
     async searchMovies({ state, commit }, payload) {
+      if (state.loading) return
+      commit('updateState', {
+        message: '',
+        loading: true
+      })
       try {
         const res = await _fetchMovie({
           ...payload,
@@ -56,6 +61,10 @@ export default {
         commit('updateState', {
           movies: [],
           message
+        })
+      } finally {
+        commit ('updateState', {
+          loading: false
         })
       }
     }
