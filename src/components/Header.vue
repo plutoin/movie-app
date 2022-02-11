5a32dcaed9976a12eba79ad8fa9a9f94abdcdaa0
plutoin <amiopy@naver.com>
2022-02-11 17:17:23 +0900

    nav path

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -34,7 +34,7 @@ export default {
         },
         {
           name: 'Movie',
-          href: '/movie/tt4520988',
+          href: '/movie/tt3783958',
           path: /^\/movie/  // '/movie'
         },
         {
