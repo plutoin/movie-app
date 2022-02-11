c47731f2d62197a2f0c15fde1d7c86366504de25
plutoin <amiopy@naver.com>
2022-02-11 16:20:20 +0900

    About

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'plutoin',
+    email: 'uleage@gmail.com',
+    blog: 'https://bit.ly/3oC0YVi',
+    phone: '+82-10-1234-5678',
+    image: 'https://user-images.githubusercontent.com/66389585/153550860-3f106406-1d07-42e8-9a7b-5ab78f63db7a.png'
+  })
+}
