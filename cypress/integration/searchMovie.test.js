8866adcc14a5de2541485849ffa5a2579fb15e6b
plutoin <amiopy@naver.com>
2022-03-05 02:02:43 +0900

    Add E2E Test

---
+++ cypress/integration/searchMovie.test.js
@@ -0,0 +1,33 @@
+describe('영화 검색(라라랜드)', () => {
+  it('검색 페이지로 접근합니다', () => {
+    cy.visit('/')
+    cy.get('header .nav-link.active')
+      .contains('Search')
+  })
+  it('영화를 검색합니다', () => {
+    cy.get('input.form-control')
+      .type('la la land')
+    cy.get('select.form-select:nth-child(2)')
+      .select('30')
+    cy.get('button.btn')
+      .contains('Apply')
+      .click()
+    cy.wait(2000)
+    cy.get('.movie')
+      .should('have.length', 30)
+  })
+  it('라라랜드 영화 아이템을 선택합니다', () => {
+    cy.get('.movie .title')
+      .contains('La La Land')
+      .click()
+  })
+  it('영화 상세 정보를 확인합니다', () => {
+    cy.url()
+      .should('include', '/movie/tt3783958')
+    cy.wait(1000)
+    cy.get('header .nav-link.active')
+      .contains('Movie')
+    cy.get('.title')
+      .contains('La La Land')
+  })
+})
